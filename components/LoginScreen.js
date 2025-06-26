323b3ce7d96501988c329b98b567cceb26a1d88e
Walter-Duchi <waltduchi@gmail.com>
2025-06-26 17:17:35 -0500

    Refactor App component to use LoginScreen and update styles; enhance LoginScreen with email and password inputs

--- components/LoginScreen.js
+++ components/LoginScreen.js
@@ -1,10 +1,33 @@
-import { ScrollView, Text, StyleSheet } from "react-native";
+import { useState } from "react";
+import { ScrollView, Text, StyleSheet, TextInput } from "react-native";
+import React from "react";
 
 export default function LoginScreen() {
+  const [email, onChangeEmail] = React.useState("");
+  const [password, onChangePassword] = React.useState("");
+
   return (
     <ScrollView style={styles.container}>
       <Text style={styles.headerText}>Welcome to Little Lemon</Text>
       <Text style={styles.regularText}>Login to continue </Text>
+      <TextInput
+        placeholder="Email Adress"
+        style={styles.textInput}
+        value={email}
+        onChange={onChangeEmail}
+        keyboardType="email-address"
+        autoCapitalize="none"
+      ></TextInput>
+
+      <TextInput
+        secureTextEntry={true}
+        placeholder="Password"
+        style={styles.textInput}
+        value={password}
+        onChange={onChangePassword}
+        autoCapitalize="none"
+        keyboardType="password"
+      ></TextInput>
     </ScrollView>
   );
 }
@@ -12,6 +35,11 @@ export default function LoginScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  textInput: {
+    backgroundColor: "#ffffff",
+    marginVertical: 8,
+    marginHorizontal: 20,
   },
   headerText: {
     padding: 40,
